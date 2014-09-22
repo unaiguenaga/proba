5a49cb8853581f4e0876d1ab060eb3a7c2b5c74a
Unai <uguenaga001@ikasle.ehu.com>
2014-09-22 20:11:06 +0200

    prueba 2 karpeta ezabatu eta hauteskundeetan mahai berri bat gehitu, nire izenarekin

--- hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -24,7 +24,7 @@ public class Hauteskundeak {
 		h2012.gehituMahaia("Irun");
 		h2012.gehituMahaia("Donostia");
 		h2012.gehituMahaia("Tolosa");
-		
+		h2012.gehituMahaia("Unai Guenaga");
 		System.out.println(h2012);
 	}
 
